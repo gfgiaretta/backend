e0edc6030f4221fd997a25900d27912f10bb3e34
Vicente Hofmeister <105060472+vicente-hofmeister@users.noreply.github.com>
2025-06-12 23:33:33 -0300

    Update src/services/comment.service.ts
    Co-authored-by: gfgiaretta <112209227+gfgiaretta@users.noreply.github.com>

--- src/services/comment.service.ts
+++ src/services/comment.service.ts
@@ -67,7 +67,6 @@ export class CommentService {
       },
     });
 
-    console.log(existingComment);
 
     if (!existingComment) {
       throw new HttpException('Comment does not exist', HttpStatus.BAD_REQUEST);
